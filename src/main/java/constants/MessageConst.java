c08a1b768ac3597629462f513c8e7a8fe10be3f8
yuuma.kimura2 <y_kimura@horizon-so.jp>
2024-06-14 10:24:29 +0900

    destroy

--- src/main/java/constants/MessageConst.java
+++ src/main/java/constants/MessageConst.java
@@ -14,7 +14,7 @@ public enum MessageConst {
     
     // DB更新
     I_REGISTERED("登録が完了しました。"),
-    I_UPDAED("更新が完了しました。"),
+    I_UPDATED("更新が完了しました。"),
     I_DELETED("削除が完了しました。"),
     
     // バリデーション
